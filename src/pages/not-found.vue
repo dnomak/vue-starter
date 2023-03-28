c87963ea16921e313911ec040fb1f5aa4c061ea8
dnomak <me@dnomak.com>
2023-03-29 02:31:52 +0300

    Add not found page

---
+++ src/pages/not-found.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'PageNotFound',
+}
+</script>
+
+<template lang="pug">
+h1 {{ $t('Page not found.') }}
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 42rem;
+  letter-spacing: -2rem;
+  line-height: 42rem;
+  font-weight: 800;
+  padding-bottom: 8rem;
+  color: var(--red-300);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/pages/home.vue'
+import NotFoundPage from '@/pages/not-found.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundPage,
     },
   ],
 })
